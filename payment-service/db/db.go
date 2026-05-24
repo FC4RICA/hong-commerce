cef9ce35d4c730354237bbb9335eec7affe955f1
Narutchai <koonfluk2015@gmail.com>
2026-05-25 00:01:45 +0700

    Add Postgres driver and related dependencies

---
+++ payment-service/db/db.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/FC4RICA/hong-commerce/payment-service/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func ConnectDB(cfg *config.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=%s",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.TimeZone,
+	)
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Warn,
+			IgnoreRecordNotFoundError: true,
+			ParameterizedQueries:      true,
+			Colorful:                  false,
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger:         newLogger,
+		TranslateError: true,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("connect to database: %w", err)
+	}
+
+	slog.Info("Database connected successfully")
+
+	if err := db.AutoMigrate(); err != nil {
+		return nil, fmt.Errorf("auto-migrate: %w", err)
+	}
+
+	return db, nil
+}
